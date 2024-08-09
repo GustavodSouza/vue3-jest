a735ec829b138a6bcd4515c8bc95cf4fd4913ff4
Gustavo de Lima <gustavo.delima@castgroup.com.br>
2024-08-09 14:41:15 -0300

    fix: config eslint

--- src/tests/helloworld.test.js
+++ src/tests/helloworld.test.js
@@ -1,30 +1,31 @@
 import HelloWorld from '@/components/HelloWorld.vue'
 import { mount, shallowMount } from "@vue/test-utils";
 
-test("HelloWorld Component renders the correct text with props", () => {
-  const wrapper = mount(HelloWorld, {
-    props: {
+describe('Group test', () => {
+  test("HelloWorld Component renders the correct text with props", () => {
+    const wrapper = mount(HelloWorld, {
+      props: {
         msg: 'Teste'
-    }
+      }
+    });
+    expect(wrapper.props().msg).toBe("Teste");
   });
-  expect(wrapper.props().msg).toBe("Teste");
-});
 
-test("HelloWorld Component getText", () => {
+  test("HelloWorld Component getText", () => {
     const wrapper = shallowMount(HelloWorld);
-    
+
     const input = 'Hello, World!';
     const expectedOutput = 'This text is: Hello, World!';
 
     const result = wrapper.vm.getText(input)
 
     expect(result).toBe(expectedOutput)
-});
+  });
 
 
-test("HelloWorld Component setText", () => {
+  test("HelloWorld Component setText", () => {
     const wrapper = shallowMount(HelloWorld);
-    
+
     const input = 'Hello, World!';
     const expectedOutput = 'Hello, World!';
 
@@ -33,4 +34,6 @@ test("HelloWorld Component setText", () => {
     const textData = wrapper.vm.text
 
     expect(textData).toBe(expectedOutput)
-});
+  });
+})
+
